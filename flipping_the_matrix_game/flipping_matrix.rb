e9f57c611b3cb2aabc7e155ac3c9eea0e1946b33
Bahar Cerit <baharyldz.c@gmail.com>
2024-04-23 10:54:59 +0200

    added test to flipping matrix method

--- flipping_the_matrix_game/flipping_matrix.rb
+++ flipping_the_matrix_game/flipping_matrix.rb
@@ -6,8 +6,6 @@ def flippingMatrix(matrix)
 
   quad_values = Array.new(n) { |r|
     Array.new(n) { |c|
-    puts "r: #{r}, c: #{c}"
-    puts "m[r][c]: #{m[r][c]}, m[r][q - 1 - c]: #{m[r][q - 1 - c]}, m[q - 1 - r][c]: #{m[q - 1 - r][c]}, m[q - 1 - r][q - 1 - c]: #{m[q - 1 - r][q - 1 - c]}"
       [m[r][c], m[r][q - 1 - c], m[q - 1 - r][c], m[q - 1 - r][q - 1 - c]].max
     }
   }
@@ -15,4 +13,3 @@ def flippingMatrix(matrix)
   return quad_values.flatten.sum
 end
 
-puts flippingMatrix([[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]]) # 414
---
+++ flipping_the_matrix_game/test.rb
@@ -0,0 +1,4 @@
+require_relative 'flipping_matrix'
+
+puts flippingMatrix([[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]]) == 414
+puts flippingMatrix([[1, 2], [3, 4]]) == 4
