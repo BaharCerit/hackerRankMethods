3866ebeddfc876fd573bc2630f26cf0cee3eff9c
Bahar Cerit <baharyldz.c@gmail.com>
2024-04-24 11:40:18 +0200

    tower breakers method added

---
+++ tower_breakers/test.rb
@@ -0,0 +1,6 @@
+require_relative 'tower_breakers'
+
+puts towerBreakers(2, 2) == 2
+puts towerBreakers(1, 4) == 1
+puts towerBreakers(2, 6) == 2
+puts towerBreakers(3, 7) == 1
---
+++ tower_breakers/tower_breakers.rb
@@ -0,0 +1,4 @@
+def towerBreakers(n, m)
+  return 2 if m == 1 || n % 2 == 0
+  return 1
+end
